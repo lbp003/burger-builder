6d85a70b202eda8398f76543e85b5e4bc397c60f
DESKTOP-KEONA0H\LBP-PC <pglbuddhika@gmail.com>
2020-09-14 00:39:02 +0530

    pharse 1

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igntKey => { 
+            return [...Array(props.ingredients[igntKey])].map((_, i) => {
+                return <BurgerIngredient key={ igntKey + i } type={ igntKey } />;
+                });
+             }).reduce((arr, el) => {
+                 return arr.concat(el);
+             }, []); 
+    if(transformedIngredients.length == 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+    return (
+    <div className={styles.Burger}>
+        <BurgerIngredient type="bread-top"></BurgerIngredient>
+        { transformedIngredients }
+        <BurgerIngredient type="bread-bottom"></BurgerIngredient>
+    </div>
+    );
+};
+
+export default Burger;
